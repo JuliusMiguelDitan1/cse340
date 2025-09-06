e21756eabedb36498fc30c52ce7099b1e76337ee
juliusmiguelditan1 <jditan@byupathway.edu>
2025-09-06 19:29:02 +0800

    asdw

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-// Index route
 app.get("/", function(req, res) {
 res.render("index", {title: "Home"})
 }) 
