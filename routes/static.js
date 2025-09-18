78e2a576bfbfef892661ab92232b2cabcc31048e
juliusmiguelditan1 <jditan@byupathway.edu>
2025-09-18 23:13:10 +0800

    432

--- routes/static.js
+++ routes/static.js
@@ -9,8 +9,10 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
-module.exports = router;
 //index route
 application.get("/",baseController.buildHome)
+
+module.exports = router;
+
 
 
