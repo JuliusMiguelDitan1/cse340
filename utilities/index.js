f81a3c8e5fae8dff0acc1ff9b373ca3a35a50911
juliusmiguelditan1 <jditan@byupathway.edu>
2025-09-18 23:46:35 +0800

    87

--- utilities/index.js
+++ utilities/index.js
@@ -4,7 +4,7 @@ const Util = {}
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
-Util.getNav = async function () {
+Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
   let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
