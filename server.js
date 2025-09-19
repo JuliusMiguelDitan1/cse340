76a0f50d645f86cd75f0f2b035af4f783f6b2595
juliusmiguelditan1 <jditan@byupathway.edu>
2025-09-19 23:34:05 +0800

    ret

--- server.js
+++ server.js
@@ -43,8 +43,7 @@ app.use("/inv", inventoryRoute)
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
 
 /* ***********************
  * Log statement to confirm server operation
